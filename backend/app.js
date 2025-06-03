d8d7d431b539945b1ee864167601f2044575097c
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-03 18:44:36 +0530

    get shorturl api endpoint is created

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,42 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import {nanoid} from 'nanoid';
-import dotenv from 'dotenv';
-dotenv.config("./.env"); 
+import { nanoid } from "nanoid";
+import dotenv from "dotenv";
+dotenv.config("./.env");
 
 import connectDB from "./src/config/mongo.config.js";
-import urlSchema from './src/models/shorturl.model.js';
-
+import urlSchema from "./src/models/shorturl.model.js";
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/create", (req,res)=>{
-   
-    res.send(nanoid(10));
+app.get("/api/create", (req, res) => {
+  res.send(nanoid(10));
+});
+
+app.get("/:id", async(req,res)=>{
+    const {id} = req.params;
+    const url = await urlSchema.findOne({short_url: id});
+    if(url){
+        res.redirect(url.full_url);
+    }
+    else{
+        res.status(404).send("URL not found");
+    }
 })
 
-app.post("/api/create", (req,res)=>{
-    const {url} = req.body;
-    const shorturl = nanoid(7)
-    const newUrl = new urlSchema({
-        full_url: url,
-        short_url: shorturl,
-    })
-    newUrl.save()
-   res.send(url);
-})
+app.post("/api/create", (req, res) => {
+  const { url } = req.body;
+  const shortUrl = nanoid(7);
+  const newUrl = new urlSchema({
+    full_url: url,
+    short_url: shortUrl,
+  });
+  newUrl.save();
+  res.send(url);
+});
 
-app.listen(3000, ()=>{
-    connectDB();
-    console.log('Server is running on port 3000');
-})
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server is running on port 3000");
+});
