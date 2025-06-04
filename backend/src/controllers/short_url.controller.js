b704691321da2d6210c7cad31c740803b21a7321
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-04 23:37:46 +0530

    try catch block is added in short url controller

--- backend/src/controllers/short_url.controller.js
+++ backend/src/controllers/short_url.controller.js
@@ -1,23 +1,27 @@
 import { getShortUrl } from "../dao/short_url.js";
-import {  createShortUrlWithoutUser } from "../services/short_url.service.js";
+import { createShortUrlWithoutUser } from "../services/short_url.service.js";
 import { generateNanoId } from "../utils/helper.js";
 
 export const createShortUrl = async (req, res) => {
-  const { url } = req.body;
+  try {
+    const { url } = req.body;
 
-  const short_url = await createShortUrlWithoutUser(url);
+    const short_url = await createShortUrlWithoutUser(url);
     if (!short_url) {
-        return res.status(500).json({ error: "Failed to create short URL" });
+      return res.status(500).json({ error: "Failed to create short URL" });
     }
     res.status(201).send(process.env.APP_URL + short_url);
+  } catch (err) {
+    console.log(err)
+    next(err);
+  }
 };
 
-
-export const redirectFromShortUrl = async(req,res)=>{
-    const {id} = req.params;
-    const url = await getShortUrl(id);
-    res.redirect(url.full_url)
-    if (!url) {
-        return res.status(404).json({ error: "Short URL not found" });
-    }
-}
+export const redirectFromShortUrl = async (req, res) => {
+  const { id } = req.params;
+  const url = await getShortUrl(id);
+  res.redirect(url.full_url);
+  if (!url) {
+    return res.status(404).json({ error: "Short URL not found" });
+  }
+};
