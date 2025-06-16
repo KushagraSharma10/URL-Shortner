e14d11fa833c33adc2cef723cb984e8a71163eb3
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-16 22:45:08 +0530

    implement user registration and login with token handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,14 +1,23 @@
 // import { registerUser, loginUser } from "../services/auth.service.js";
-import { registerUser } from "../services/auth.service.js";
+import { cookieOptions } from "../config/config.js";
+import { registerUser, loginUser } from "../services/auth.service.js";
 import wrapAsync from "../utils/tryCatchWrapper.js";
 
 export const register_user = wrapAsync(async (req, res) => {
   const {name, email, password}  = req.body;
-  const user = await registerUser(name, email, password);
-  
-  res.send("Register");
+  const token  = await registerUser(name, email, password);
+  if (!token) {
+    return res.status(500).json({ error: "Failed to register user" });
+  }
+  res.cookie("accessToken", token, cookieOptions);
+  res.status(200).json({message:"User registered successfully"});
 });
 
 export const login_user = wrapAsync(async (req, res) => {
-  res.send("Login");
+  const { email, password } = req.body;
+  const token = await loginUser(email, password);
+  if (!token) return res.status(401).json({ error: "Invalid credentials" });
+  
+  res.cookie("accessToken", token, cookieOptions);
+  res.status(200).json({message:"User logged in successfully"});
 });
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,13 +1,23 @@
-import { createUser, findUserByEmail } from "../dao/user.dao.js";
-import { signToken } from "../utils/helper.js";
-
+import { createUser, findUserByEmail, findUserById } from "../dao/user.dao.js";
+import { signToken, verifyToken } from "../utils/helper.js";
 
 export const registerUser = async (name, email, password) => {
-    const user = await findUserByEmail(email);
-    if (user) {
-      throw new Error("User already exists");
-    }
-    const newUser = await createUser(name, email, password);
-    const token = signToken({ id: newUser._id });
-    return token;
-  };
+  const user = await findUserByEmail(email);
+  if (user) {
+    throw new Error("User already exists");
+  }
+  const newUser = await createUser(name, email, password);
+  const token = signToken({ id: newUser._id });
+  return token;
+};
+
+export const loginUser = async (email, password) => {
+  const user = await findUserByEmail(email);
+
+  if (!user || user.password !== password) {
+    throw new Error("Invalid credentials");
+  }
+
+  const token = signToken({ id: user._id });
+  return token;
+};
